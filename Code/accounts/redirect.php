ef4d01b38d807be0b7ea3cf039dc7db1ff61c41c
fayedu39 <fayedu39@live.fr>
2014-10-01 18:32:05 +0200

    Code
    Ajout d'un .htaccess, début d'un système d'inclusion de page... C'est un
    peu le bordel.

--- Code/accounts/redirect.php
+++ Code/accounts/redirect.php
@@ -1,3 +1,6 @@
 <?php
-
-	header("location:./../");
+	
+	$redirectPage="./../";
+	if($_SERVER["HTTP_REFERER"])
+		$redirectPage=$_SERVER["HTTP_REFERER"];
+	header("location:".$redirectPage);
--- Code/index.php
+++ Code/index.php
@@ -2,5 +2,5 @@
 	session_start();
 	define( 'ABSPATH', dirname(__FILE__) . '/' );
 
-	require(ABSPATH."includes/config.php");
-?><!DOCTYPE html><html lang="fr">	<head>		<meta charset="utf-8"/>		<style type="text/css">			<?php								require(ABSPATH."templates/common.css");							?>		</style>		<title>RP-Project</title>	</head>	<body>		<?php			require(ABSPATH."templates/background.php");			require(ABSPATH."templates/header.php");		?>		<?php			require(ABSPATH."templates/footer.php");		?>	</body></html>
+	require(ABSPATH."includes/config.php");		$pageFile="not_found.php";		if(isset($_GET["page"])) {		switch($_GET["page"]) {			case "index.html":				$pageFile="index.php";			break;		}	} else {		$pageFile="index.php";	}
+?><!DOCTYPE html><html lang="fr">	<head>		<meta charset="utf-8"/>		<style type="text/css">			<?php								require(ABSPATH."templates/common.css");							?>		</style>		<title>RP-Project</title>	</head>	<body>		<?php			require(ABSPATH."templates/background.php");			require(ABSPATH."templates/header.php");		?>		<?php			require(ABSPATH."pages/".$pageFile);		?>		<?php			require(ABSPATH."templates/footer.php");		?>	</body></html>
