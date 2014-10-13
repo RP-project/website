9af142ba05a4c0fe2546c0cdd53fbf067e0fd81b
fayedu39 <fayedu39@live.fr>
2014-10-13 21:03:47 +0200

    Mise en forme
    Début mise en forme de l'accueil+favicon

--- Code/pages/index.php
+++ Code/pages/index.php
@@ -1,5 +1,19 @@
-<div id="body">
-	<?php
-		
-	?>
+<div class="homeBoxContainer">
+	<div class="homeBox block">
+		<h2>Dernières news</h2>
+		<div>À venir</div>
+	</div>
+	<div class="homeBox block">
+		<h2>Derniers récits</h2>
+	</div>
+</div>
+<div class="homeBoxContainer">
+	<div class="homeBox block">
+		<h2>Derniers topics</h2>
+		<div>À venir</div>
+	</div>
+	<div class="homeBox block">
+		<h2>Top artworks</h2>
+		<div>À venir</div>
+	</div>
 </div>
